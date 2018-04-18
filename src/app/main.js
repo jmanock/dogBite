c7b4df1dba673b3b2004e30cb9414a282e0aeda6
jManock <jonmanock@gmail.com>
2018-04-18 15:31:13 -0400

    header works with hide show

--- src/app/main.js
+++ src/app/main.js
@@ -3,5 +3,31 @@ module.exports = {
   controller: MainCtrl
 };
 
-function MainCtrl() {
+function MainCtrl($scope, $log) {
+  $scope.click = function (x) {
+    if (x === 'about') {
+      $scope.home = true;
+      $scope.about = true;
+      $scope.questions = false;
+      $scope.quote = false;
+    } else if (x === 'quote') {
+      $scope.home = true;
+      $scope.about = false;
+      $scope.quote = true;
+      $scope.questions = false;
+    } else if (x === 'home') {
+      $scope.home = false;
+      $scope.about = false;
+      $scope.quote = false;
+      $scope.questions = false;
+    } else {
+      $scope.home = true;
+      $scope.about = false;
+      $scope.quote = false;
+      $scope.questions = true;
+    }
+  };
+  $scope.top = function (y) {
+    $log.log(y);
+  };
 }
