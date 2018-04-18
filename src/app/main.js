0852e1c7de94425230cca3711c8179ff53906098
jManock <jonmanock@gmail.com>
2018-04-18 15:54:24 -0400

    header and footer links work

--- src/app/main.js
+++ src/app/main.js
@@ -3,7 +3,7 @@ module.exports = {
   controller: MainCtrl
 };
 
-function MainCtrl($scope, $log) {
+function MainCtrl($scope) {
   $scope.click = function (x) {
     if (x === 'about') {
       $scope.home = true;
@@ -28,6 +28,48 @@ function MainCtrl($scope, $log) {
     }
   };
   $scope.top = function (y) {
-    $log.log(y);
+    if (y === 'home') {
+      $scope.home = false;
+      $scope.about = false;
+      $scope.questions = false;
+      $scope.privacy = false;
+      $scope.quote = false;
+      $scope.terms = false;
+    } else if (y === 'about') {
+      $scope.about = true;
+      $scope.home = true;
+      $scope.questions = false;
+      $scope.privacy = false;
+      $scope.quote = false;
+      $scope.terms = false;
+    } else if (y === 'questions') {
+      $scope.questions = true;
+      $scope.home = true;
+      $scope.about = false;
+      $scope.privacy = false;
+      $scope.quote = false;
+      $scope.terms = false;
+    } else if (y === 'privacy') {
+      $scope.privacy = true;
+      $scope.home = true;
+      $scope.about = false;
+      $scope.questions = false;
+      $scope.quote = false;
+      $scope.terms = false;
+    } else if (y === 'quote') {
+      $scope.quote = true;
+      $scope.home = true;
+      $scope.about = false;
+      $scope.questions = false;
+      $scope.privacy = false;
+      $scope.terms = false;
+    } else {
+      $scope.terms = true;
+      $scope.about = false;
+      $scope.home = true;
+      $scope.questions = false;
+      $scope.privacy = false;
+      $scope.quote = false;
+    }
   };
 }
